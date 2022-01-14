7665c35a96bf29693eb67958cd379e945d70314f
Henrique Daniel <henriquec.daniel@hotmail.com>
2022-01-13 23:09:13 -0300

    fcc projects (html + css)

---
+++ fcc-projects.py
@@ -0,0 +1,12 @@
+# projetos básicos usando html + css para construção dos desafios do curso de responsive web design da freecodecamp
+# links codepen
+
+# personal portfolio - https://codepen.io/henriquecd-tech/pen/abmoGBz
+
+# technical documentation page - https://codepen.io/henriquecd-tech/pen/ExyBejK
+
+# product landing page - https://codepen.io/henriquecd-tech/pen/BazeaeM
+
+# survey form - https://codepen.io/henriquecd-tech/pen/JjKepYE
+
+# tribute page - https://codepen.io/henriquecd-tech/pen/VwjBJbe
